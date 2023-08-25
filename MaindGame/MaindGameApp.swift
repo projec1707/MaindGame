1525034bf89f02cf7580cff38819412f94005c4a
projec1707 <pwroma@gmail.com>
2023-08-25 20:19:25 +0300

    Initial Commit

---
+++ MaindGame/MaindGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaindGameApp.swift
+//  MaindGame
+//
+//  Created by Roman Riepa on 25.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MaindGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
