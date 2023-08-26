41c16d84ae9accde645c0d26f1b03267890f0fd4
projec1707 <pwroma@gmail.com>
2023-08-26 18:55:41 +0300

    isFaceUp togglable

--- MaindGame/ContentView.swift
+++ MaindGame/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         HStack{
             
-                CardView(isFaceUp: true)
+                CardView()
                 CardView()
                 CardView()
            
@@ -24,19 +24,21 @@ struct ContentView: View {
 
 struct CardView: View {
     
-    var isFaceUp: Bool = false
+    @State private var isFaceUp: Bool = false
     var body: some View {
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundStyle(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("👻")
                     .font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
